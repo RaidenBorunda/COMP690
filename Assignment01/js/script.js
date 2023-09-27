6219fa2b681c750d424732004a461cfb6410841d
RaidenBorunda <137729767+RaidenBorunda@users.noreply.github.com>
2023-09-26 20:14:10 -0700

    Assignment 01

---
+++ Assignment01/js/script.js
@@ -0,0 +1,115 @@
+// STEP 1
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// STEP 2
+// 3.33 // Numeric Expression
+// "This is a string" // String literal Expression
+// false // Boolean Literal Expression
+// null // Null Literal Expression
+
+// STEP 3
+// let x = 1 + 2 * 3
+// let a = 10 ; let b = 20 ; alert(a == 3 || a == 10)
+
+// STEP 4
+// let firstName
+// let lastName
+// let address
+// let city
+// let state
+// let lZipCode
+// let lYourAge
+// let referralSource
+// let mayWeContactYou
+
+// STEP 5
+// let firstName = 'Raiden'
+// var lYourAge = '29'
+// const lZipCode = '92027'
+
+// STEP 6
+// let firstCar = 1995 + ' Mazda Miata'
+// console.log(firstCar)
+
+// STEP 7
+// let favoriteColor = true + ' Blue'
+// console.log(favoriteColor)
+// let boilingWater = false + 112
+// console.log(boilingWater)
+
+// STEP 8
+// No, I would correct it to:
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// STEP 9
+// let x = null
+// console.log(x)
+// let y 
+// console.log(y)
+
+// STEP 10
+// console.log(typeof 7)
+// console.log(typeof false)
+// console.log(typeof 'Is this a string?')
+// console.log(typeof Object())
+// console.log(typeof x)
+
+// STEP 11
+// alert('Hello ' + 'Raiden Borunda'  + ', welcome to the JavaScript class!')
+
+// STEP 12
+// let name = 'Raiden Borunda'
+// alert(`Hello ${name}, welcome to the JavaScript class!`)
+
+// STEP 13
+// let name = 'Raiden Borunda'
+// let course = 'JavaScript'
+// alert(`Hello ${name}, welcome to the ${course} class!`)
+
+// STEP 14
+// let name = 'Raiden Borunda'
+// let course = 'JavaScript'
+// alert(`Hello ${name}. \nWelcome to the ${course} class!`)
+
+// STEP 15
+// let name = prompt('Please enter your name.')
+// alert(`Hello ${name}, welcome to the JavaScript class!`)
+
+// STEP 16
+// let course = prompt('Please enter the class name.')
+// alert(`Hello Raiden Borunda, welcome to the ${course} class.`)
+
+// STEP 17
+// let x = 10
+// let y = 20
+// console.log(x + y)
+
+// STEP 18
+// let x = 20
+// x = x + 20
+// console.log(x)
+
+// STEP 19
+// let x = 20
+// x = x * 5
+// console.log(x)
+
+// STEP 20
+// let x = 20 % 3
+// x = x / 1
+// console.log(x)
+
+// STEP 21
+// let x = 10
+// let y = 5
+// console.log(x >= y && y == 5)
+
+// STEP 22
+// let a = 3
+// let b = 9
+// console.log( a <= 1 || b === 'str')
