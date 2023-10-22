8cf0e8c548f5d525516e2790369fd5f1e07501a4
RaidenBorunda <137729767+RaidenBorunda@users.noreply.github.com>
2023-10-22 09:09:48 -0700

    Assignment 05a

--- Assignment05/js/script.js
+++ Assignment05/js/script.js
@@ -1,16 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("empForm").addEventListener("submit", (e) => {
         e.preventDefault();
-
+       
         let id          = document.getElementById('id').value
-        let name    = document.getElementById('name').value
+        let name        = document.getElementById('name').value
         let ext         = document.getElementById('ext').value
         let email       = document.getElementById('email').value
         let department  = document.getElementById('department').value
-
         console.log("ID: ", id);
         console.log("Name: ", name);
         console.log("Extension: ", ext);
         console.log("Email: ", email);
         console.log("Department: ", department);  
-    })})
+})})
