c5b52644102097c84ad78a276ee864aa276d4fb3
RaidenBorunda <137729767+RaidenBorunda@users.noreply.github.com>
2024-01-10 07:42:33 -0800

    M11 Assign.

--- M11 Assignment/js/dates.js
+++ M11 Assignment/js/dates.js
@@ -2,24 +2,24 @@
 function daysInMonth(month, year){
     return new Date (year, month, 0).getDate();
 }
-var month = prompt("Please enter a month in numeric value. For example, October is 10.")
+let month = prompt("Please enter a month in numeric value. For example, October is 10.")
 console.log(daysInMonth(month, 2023));
 //STEP 2
 function monthName(year, month, day){
-    var date = new Date(year, month, day);
-    var monthy = date.toLocaleString('default', { month: 'long'} );
+    let date = new Date(year, month, day);
+    let monthy = date.toLocaleString('default', { month: 'long'} );
     console.log(monthy)
 }
-var year = prompt("Please enter a year.");
-var month = prompt("Please enter a month in numeric value. For example, March is 03.");
-var day = prompt("Please enter a day in numeric value.");
+let year = prompt("Please enter a year.");
+let month = prompt("Please enter a month in numeric value. For example, March is 03.");
+let day = prompt("Please enter a day in numeric value.");
 month = month - 1 
 monthName(year, month, day)
 //STEP 3
-var datePrompt = new Date(prompt('Please enter a date like 2023-01-15'));
-var currentDay = datePrompt.getDay();
+let datePrompt = new Date(prompt('Please enter a date like 2023-01-15'));
+let currentDay = datePrompt.getDay();
 function weekendChecker(){
-    var dateIsWeekend = (currentDay === 6) || (currentDay === 0);
+    let dateIsWeekend = (currentDay === 6) || (currentDay === 0);
     if(dateIsWeekend == true) {
         console.log("The given date " + datePrompt + " is a weekend.")
     } else {
@@ -29,14 +29,14 @@ function weekendChecker(){
 weekendChecker()
 //STEP 4
 function yesterdayCheck(){
-    var d = new Date()
+    let d = new Date()
     d.setDate(d.getDate() - 1)
     console.log(d.toLocaleString('default', { weekday: 'long'}))
 }
 yesterdayCheck()
 //STEP 5
 function dateFirstLetter(){
-    var d = new Date()
+    let d = new Date()
     d = d.toLocaleString('default', { weekday: 'long'})
     console.log(d.charAt(0))
 }
--- M11 Assignment/js/deathbyjs.js
+++ M11 Assignment/js/deathbyjs.js
@@ -1,39 +1,39 @@
 //STEP 1
-var str = prompt("Please enter a random word.");
+let str = prompt("Please enter a random word.");
 str = str.toLowerCase();
 function strSorter(str){
-    var chars = str.split('').sort().join('');
+    let chars = str.split('').sort().join('');
     console.log(chars)
 }
 strSorter(str)
 //STEP 2
-var sentence = prompt("Please enter a sentence.")
+let sentence = prompt("Please enter a sentence.")
 
 function strUpperCase(sentence){
-    var words = sentence.split(" ");
+    let words = sentence.split(" ");
 
     for (let i = 0; i < words.length; i++) {
         words[i] = words[i][0].toUpperCase() + words[i].substr(1);
         
     } 
-    var str = words.join(' ');
+    let str = words.join(' ');
     console.log(str)
 }
 strUpperCase(sentence)
 //STEP 3
-var str = prompt("Please enter another sentence.");
+let str = prompt("Please enter another sentence.");
 function vowelCounter(str){
-    var vowels = (str.toLowerCase().match(/[a,e,i,o,u]/g) || '').length
+    let vowels = (str.toLowerCase().match(/[a,e,i,o,u]/g) || '').length
     console.log("There are " + vowels + " vowels in your sentence.");
 }
 vowelCounter(str);
 
 //STEP 4
-var stringLength = prompt("Please enter a random number.")
+let stringLength = prompt("Please enter a random number.")
 
 function randomString(stringLength){
-    var randomString = '';
-    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz0123456789'
+    let randomString = '';
+    let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz0123456789'
     for (let i = 1; i < stringLength; i++) {
         let char = Math.floor(Math.random() * randomChars.length +1)
         randomString += randomChars.charAt(char)
@@ -43,9 +43,9 @@ function randomString(stringLength){
 
 console.log(randomString(stringLength))
 //STEP 5
-var countryPrompt = prompt("Please enter 3 country names separated by commas.")
+let countryPrompt = prompt("Please enter 3 country names separated by commas.")
 function longestCountry(countryPrompt){
-    var words = countryPrompt.split(", ");
+    let words = countryPrompt.split(", ");
     return words.sort(function (a, b) {
         return b.length - a.length;
     })[0];
